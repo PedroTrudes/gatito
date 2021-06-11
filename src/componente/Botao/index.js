05f080dc67397b34c7c0e2231296e0248c866e5c
Pedro Trudes <pedro.jtrudes@gmail.com>
2021-06-11 00:44:33 -0300

    Ajustando componentes
    Criando componentes de botão e estilos

---
+++ src/componente/Botao/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+
+export default function Botao({valor, acao}) {
+    return <TouchableOpacity onPress={acao}>
+        <Text>{valor}</Text>
+    </TouchableOpacity>
+
+}
--- src/telas/Servicos/item/index.js
+++ src/telas/Servicos/item/index.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View} from 'react-native';
+import Botao from '../../../componente/Botao';
+import CampoInteiro from '../../../componente/CampoInteiro';
+import estilos from './estilos';
+
 
 export default function Item({nome, preco, descricao}) {
-    return <>
-        <Text>{nome}</Text>
-        <Text>{preco}</Text>
-        <Text>{descricao}</Text>
-    </>
+    const [quantidade, setQuantidade] = useState(1);
 
+    return <>
+    <View style={estilos.informacao}>
+        <Text style={estilos.nome}>{ nome }</Text>
+        <Text style={estilos.descricao}>{ descricao }</Text>
+        <Text style={estilos.preco}>{ preco }</Text>
+    </View>
+    <View style={estilos.carrinho}>
+        <View>
+            <View style={estilos.valor}>
+                <Text style={estilos.quantidade}>Quantidade:</Text>
+                <CampoInteiro estilos={estilos.quantidade}  valor={quantidade} acao={setQuantidade} />
+            </View>
+            <View style={estilos.valor}>
+                <Text style={estilos.descricao}>Preço:</Text>
+                <Text style={estilos.preco}>0</Text>
+            </View>
+        </View>
+        <Button valor="Adicionar" acao={() => {}} />
+    </View>
+    <View style={estilos.divisor} />
+    </>
 }
